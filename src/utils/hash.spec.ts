872a820708d64655a763e67324f9c75bfad44407
Guja Mtchedlidze <emtchedlidze@unisens.ge>
2022-02-01 20:53:25 +0400

    added one test for hashPassword

---
+++ src/utils/hash.spec.ts
@@ -0,0 +1,29 @@
+import { Test } from '@nestjs/testing'
+import { HashPassword } from './hash'
+
+describe('hash password with salt', () => {
+  let hashPassword: HashPassword
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [],
+      providers: [HashPassword],
+    }).compile()
+
+    hashPassword = moduleRef.get<HashPassword>(HashPassword)
+  })
+
+  describe('should hash password with salt', () => {
+    const pass = 'secret'
+    let salt, password
+    beforeEach(async () => {
+      const secrets = await hashPassword.hash(pass)
+      console.log(secrets)
+      salt = secrets.salt
+      password = secrets.password
+    })
+    test('password should be hashed, salt should be defined', () => {
+      expect(pass !== password)
+      expect(salt).toBeDefined()
+    })
+  })
+})
